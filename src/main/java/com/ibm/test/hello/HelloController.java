076abaec959625932a123c022c5510141efcd121
Edmundas Ramanauskas <edmundas.ramanauskas@gmail.com>
2016-05-17 19:10:26 +0300

    Implement /hello endpoint

--- src/main/java/com/ibm/test/hello/HelloController.java
+++ src/main/java/com/ibm/test/hello/HelloController.java
@@ -3,6 +3,7 @@ package com.ibm.test.hello;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -23,5 +24,10 @@ public class HelloController {
     @RequestMapping(value = "/helloworld", method = RequestMethod.GET)
     public HelloModel helloWorld() {
         return helloService.sayHello(DEFAULT_NAME);
+    }
+
+    @RequestMapping(value = "/hello", method = RequestMethod.POST)
+    public HelloModel hello(@RequestParam(value="name", defaultValue=DEFAULT_NAME) String name) {
+        return helloService.sayHello(name);
     }
 }
