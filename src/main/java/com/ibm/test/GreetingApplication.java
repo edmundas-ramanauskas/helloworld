7f59a7c47feb7b6f31cfb4f7e820656f053d65e6
Edmundas Ramanauskas <edmundas.ramanauskas@gmail.com>
2016-05-17 18:42:08 +0300

    Minor improvements

--- src/main/java/com/ibm/test/GreetingApplication.java
+++ src/main/java/com/ibm/test/GreetingApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloworldApplication {
+public class GreetingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloworldApplication.class, args);
+		SpringApplication.run(GreetingApplication.class, args);
 	}
 }
