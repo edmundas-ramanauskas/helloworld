f1c6057aab00b74609960311e5d4477f7528b2a5
Edmundas Ramanauskas <edmundas.ramanauskas@gmail.com>
2016-05-17 18:42:49 +0300

    Add test for /helloworld endpoint

---
+++ src/test/java/com/ibm/test/hello/HelloControllerTest.java
@@ -0,0 +1,48 @@
+package com.ibm.test.hello;
+
+import com.ibm.test.GreetingApplication;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by edmundas on 16.5.17.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = GreetingApplication.class)
+@WebAppConfiguration
+public class HelloControllerTest {
+
+    private static final String CONTENT_TYPE = "application/json;charset=UTF-8";
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void testHelloWorld() throws Exception {
+        this.mockMvc.perform(get("/helloworld").accept(MediaType.parseMediaType(CONTENT_TYPE)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(CONTENT_TYPE))
+                .andExpect(jsonPath("$.message").value("Hello World !"));
+    }
+}
