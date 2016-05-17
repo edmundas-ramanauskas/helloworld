a14d5e251bfd801d6e43d9e979349c407ce96cd8
Edmundas Ramanauskas <edmundas.ramanauskas@gmail.com>
2016-05-17 21:59:05 +0300

    Add logging

--- src/main/java/com/ibm/test/hello/HelloController.java
+++ src/main/java/com/ibm/test/hello/HelloController.java
@@ -1,5 +1,7 @@
 package com.ibm.test.hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,8 @@ import java.io.IOException;
 @RestController
 public class HelloController {
 
+    private final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     private static final String DEFAULT_NAME = "World";
 
     private HelloService helloService;
@@ -28,16 +32,19 @@ public class HelloController {
 
     @RequestMapping(value = "/helloworld", method = RequestMethod.GET)
     public HelloModel helloWorld() {
+        logger.info("GET /helloworld");
         return helloService.sayHello(DEFAULT_NAME);
     }
 
     @RequestMapping(value = "/hello", method = RequestMethod.POST)
     public HelloModel hello(@RequestParam(value="name", defaultValue=DEFAULT_NAME) String name) {
+        logger.info("POST /hello");
         return helloService.sayHello(name);
     }
 
     @RequestMapping(value = "/hellokitty", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
     public byte[] helloKitty() throws IOException {
+        logger.info("GET /hellokitty");
         return kittyService.getKitty();
     }
 }
